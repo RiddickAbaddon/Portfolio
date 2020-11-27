3fc6464b45882f15cbf94d8a851fafd4c9011a95
Marcin Kalinowski <marcin36k@outlook.com>
2020-11-27 06:47:51 +0100

    Correctet BackgroundSection Component

--- src/components/atoms/BackgroundSection/BackgroundSection.js
+++ src/components/atoms/BackgroundSection/BackgroundSection.js
@@ -4,27 +4,42 @@ import bgTop from 'assets/backgrounds/bgTop.png'
 import styled, { css } from 'styled-components'
 
 const BackgroundSection = styled.div`
-   background-size: 100% auto;
-   background-repeat: no-repeat;
-   ${({ background }) => {
-      switch (background) {
-         case 'center':
-            return css`
-               background-image: url(${bgCenter});
-               background-position: center center;
-            `
-         case 'bottom':
-            return css`
-               background-image: url(${bgBottom});
-               background-position: center bottom;
-            `
-         default:
-            return css`
-               background-image: url(${bgTop});
-               background-position: center top;
-            `
-      }
-   }}
+   position: relative;
+   min-height: 100vh;
+
+   ::before {
+      content: '';
+      position: absolute;
+      width: 100%;
+      height: 75vw;
+      background-size: 100% auto;
+      background-repeat: no-repeat;
+      z-index: -1;
+
+      ${({ background }) => {
+         switch (background) {
+            case 'center':
+               return css`
+                  background-image: url(${bgCenter});
+                  top: 50%;
+                  transform: translateY(-50%);
+                  background-position: center;
+               `
+            case 'bottom':
+               return css`
+                  background-image: url(${bgBottom});
+                  bottom: 0;
+                  background-position: center bottom;
+               `
+            default:
+               return css`
+                  background-image: url(${bgTop});
+                  top: 0;
+                  background-position: center top;
+               `
+         }
+      }}
+   }
 `
 
 export default BackgroundSection
--- src/components/atoms/BackgroundSection/BackgroundSection.stories.js
+++ src/components/atoms/BackgroundSection/BackgroundSection.stories.js
@@ -4,8 +4,44 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
-   max-width: 500px;
+   max-width: 700px;
+   margin: auto;
 `
+
+const Separator = styled.div`
+   padding-top: 100px;
+`
+
+const TestContent = () => (
+   <Wrapper>
+      <Separator />
+      <p>
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis felis lectus, consequat ac
+         elementum non, placerat sodales quam. Nullam malesuada tristique velit vel volutpat.
+         Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
+         egestas. Fusce sit amet odio malesuada, finibus ex id, semper leo. Suspendisse convallis
+         turpis ut euismod semper. Nam volutpat tempus est in blandit. Aliquam sit amet libero vel
+         tellus semper sagittis. In in porta massa.
+      </p>
+      <p>
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis felis lectus, consequat ac
+         elementum non, placerat sodales quam. Nullam malesuada tristique velit vel volutpat.
+         Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
+         egestas. Fusce sit amet odio malesuada, finibus ex id, semper leo. Suspendisse convallis
+         turpis ut euismod semper. Nam volutpat tempus est in blandit. Aliquam sit amet libero vel
+         tellus semper sagittis. In in porta massa.
+      </p>
+      <p>
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis felis lectus, consequat ac
+         elementum non, placerat sodales quam. Nullam malesuada tristique velit vel volutpat.
+         Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
+         egestas. Fusce sit amet odio malesuada, finibus ex id, semper leo. Suspendisse convallis
+         turpis ut euismod semper. Nam volutpat tempus est in blandit. Aliquam sit amet libero vel
+         tellus semper sagittis. In in porta massa.
+      </p>
+      <Separator />
+   </Wrapper>
+)
 
 export default {
    component: BackgroundSection,
@@ -22,41 +58,7 @@ export default {
 
 export const Basic = ({ background }) => (
    <BackgroundSection background={background}>
-      <Wrapper>
-         <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis felis lectus, consequat ac
-            elementum non, placerat sodales quam. Nullam malesuada tristique velit vel volutpat.
-            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
-            egestas. Fusce sit amet odio malesuada, finibus ex id, semper leo. Suspendisse convallis
-            turpis ut euismod semper. Nam volutpat tempus est in blandit. Aliquam sit amet libero
-            vel tellus semper sagittis. In in porta massa.
-         </p>
-         <p>
-            Cras id mauris est. Duis et ex condimentum, ultrices lectus at, venenatis est. Aenean id
-            finibus risus. Maecenas consectetur erat eget arcu molestie tristique. Lorem ipsum dolor
-            sit amet, consectetur adipiscing elit. Suspendisse imperdiet, orci mattis ullamcorper
-            fringilla, felis est suscipit nunc, in venenatis libero libero lacinia nisl. Maecenas
-            rutrum porta arcu, sit amet fringilla sem sodales quis. Ut ex felis, luctus non nunc at,
-            scelerisque pulvinar dui. Class aptent taciti sociosqu ad litora torquent per conubia
-            nostra, per inceptos himenaeos. Praesent interdum congue dolor et tincidunt. Donec
-            hendrerit commodo tortor eu semper. Curabitur ac tempus ex. Suspendisse tincidunt nunc
-            et lacus vehicula sagittis. Praesent ante ligula, sollicitudin vel volutpat eget,
-            efficitur pharetra augue. Mauris aliquam tortor dolor, nec faucibus turpis feugiat
-            vitae.
-         </p>
-         <p>
-            Nunc pellentesque efficitur suscipit. Phasellus quis dignissim arcu. Sed aliquet orci
-            purus, a viverra quam ornare sed. Sed eu nibh metus. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit. Cras at turpis justo. Phasellus finibus quam mauris, et
-            tempor dolor facilisis feugiat. Mauris felis ipsum, pulvinar sed eleifend in, volutpat
-            et quam. Cras ultrices est at luctus sagittis. Mauris condimentum sapien ac turpis
-            ornare mattis. Ut quis massa sit amet sem tempor tristique sed eu eros. Maecenas
-            venenatis venenatis interdum. Donec egestas, nulla eget lacinia fringilla, lorem metus
-            elementum magna, non ultrices dui magna vel lorem. Nulla non laoreet risus. Praesent id
-            ante aliquet, molestie elit vitae, malesuada turpis. Cras quis nibh quis elit tincidunt
-            viverra non interdum magna.
-         </p>
-      </Wrapper>
+      <TestContent />
    </BackgroundSection>
 )
 
@@ -67,3 +69,17 @@ Basic.propTypes = {
 Basic.args = {
    background: 'top',
 }
+
+export const Sections = () => (
+   <>
+      <BackgroundSection background="top">
+         <TestContent />
+      </BackgroundSection>
+      <BackgroundSection background="center">
+         <TestContent />
+      </BackgroundSection>
+      <BackgroundSection background="bottom">
+         <TestContent />
+      </BackgroundSection>
+   </>
+)
